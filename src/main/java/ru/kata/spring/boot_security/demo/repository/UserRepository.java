6afeb14c0c843b79188ecca10eb2af317bf7b135
Denis Bikkuzin <bikkuzin.den@gmail.com>
2024-06-13 20:57:21 +0500

    1_4

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -2,10 +2,11 @@ package ru.kata.spring.boot_security.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.kata.spring.boot_security.demo.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "select u from User u left join fetch u.roles where u.username=:login")
-    User findByUsername(String username);
+    User findByUsername(@Param("username") String username);
 }
